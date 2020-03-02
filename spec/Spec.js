a4efd6bd0d212abdd338256f422629f1e7e6b234
“Alexandra <alex.muskovszky@gmail.com>
2020-03-02 19:10:46 +0100

    Day1

--- spec/Spec.js
+++ spec/Spec.js
@@ -1,22 +1,25 @@
-describe('Dot class', () => {
+describe('`Dot` class', () => {
     beforeEach(function () {
         dot = new Dot();
     });
 
     describe('constructor function', () => {
-        it('should declare a color property as part of Dot', () => {
+        it('should declare a `color` property', () => {
             expect(dot.color).toBeDefined();
         });
-        it('should be a string type', () => {
+        it('`color` property should be a string type', () => {
             expect(typeof dot.color).toEqual('string');
         });
-        it('selected should be false at start', () => {
+        it('should declare a `selected` property', () => {
+            expect(dot.selected).toBeDefined();
+        });
+        it('`selected` property should be false at start', () => {
             expect(dot.selected).toEqual(false)
         });
     });
 
-    describe('setRandomColor function', () => {
-        it('declare a setRandom method', () => {
+    describe('`setRandomColor` function', () => {
+        it('declare a `setRandomColor` method', () => {
             expect(typeof dot.setRandomColor).toEqual('function');
         });
         it('should select a random color from the available colors', () => {
@@ -24,19 +27,19 @@ describe('Dot class', () => {
             expect(dot.availableColors.includes(dot.color)).toEqual(true);
         });
     });
-    describe('getColor function', () => {
-        it('declare a getColor method', () => {
+    describe('`getColor` function', () => {
+        it('declare a `getColor` method', () => {
             expect(typeof dot.getColor).toEqual('function');
         });
         it('should give back the color', () => {
             expect(typeof dot.getColor()).toEqual('string');
         });
     });
-    describe('setSelected function', () => {
-        it('declare a setSelected method', () => {
+    describe('`setSelected` function', () => {
+        it('declare a `setSelected` method', () => {
             expect(typeof dot.setSelected).toEqual('function');
         });
-        it('should change the boolean of selected', () => {
+        it('should change `selected`(boolean)', () => {
             if (dot.selected === false) {
                 dot.setSelected();
                 expect(dot.selected).toEqual(true)
@@ -47,6 +50,63 @@ describe('Dot class', () => {
         });
     });
 });
+describe('`Board` class', () => {
+    let board = new Board();
+    // beforeEach(function () {
+        
+    // });
+    describe('constructor function', () => {
+        it('should declade a `boardSize` propery', () => {
+            expect(board.boardSize).toBeDefined();
+        });
+        it('should declade a `dots` propery', () => {
+            expect(board.dots).toBeDefined();
+            expect(board.dots.length).toEqual(board.boardSize);
+            expect(board.dots[0].length).toEqual(board.boardSize);
+            expect(board.dots[0][1] instanceof Dot).toEqual(true);
+        });
+    });
+    describe('removeDot` function', () => {
+        it('declare a `removeDot` method', () => {
+            expect(typeof board.removeDot).toEqual('function');
+        });
+        it('should delete a dot', () => {
+            // expect(board.removeDot(1,2)).toEqual(null)
+        });
+    });
+
+    describe('slideDownFrom` function', () => {
+        it('declare a `slideDownFrom` method', () => {   
+            expect(typeof board.slideDownFrom).toEqual('function');
+        });
+        it('should slide down all dots above', () => {
+            let toBeSlided = board.dots[0][4];
+            let toBeSlided2 = board.dots[1][4];
+            board.slideDownFrom(2,4);
+
+            expect(toBeSlided.dotSerialNumber).toBe(board.dots[1][4].dotSerialNumber);
+            expect(toBeSlided2.dotSerialNumber).toBe(board.dots[2][4].dotSerialNumber);
+            
+            let toBeSlided3 = board.dots[0][2];
+            let toBeSlided4 = board.dots[1][2];
+            let toBeSlided5 = board.dots[2][2];
+           
+            board.slideDownFrom(3,2);
+
+            expect(toBeSlided3).toBe(board.dots[1][2]);
+            expect(toBeSlided4).toBe(board.dots[2][2]);
+            expect(toBeSlided5).toBe(board.dots[3][2]);
+
+        });
+    });
+    describe('`isAdjesent` function', () => {
+        it('should be true of adjesent dots', () => {
+            expect(board.isAdjacentTo(2,2, 4,4)).toBe(false);
+            expect(board.isAdjacentTo(2,2, 2,3)).toBe(true);
+        });
+    });
+});
+
 
 // describe('validMoves', () => {
 //     it("should be defined", ()=> {
--- src/logic.js
+++ src/logic.js
@@ -3,8 +3,9 @@ class Dot {
         this.availableColors = ['red', 'blue']
         this.color = "";
         this.setRandomColor();
-
         this.selected = false;
+        Dot.prototype.dotCount += 1;
+        this.dotSerialNumber = Dot.prototype.dotCount;
     }
     setRandomColor() {
         this.color = this.availableColors[Math.floor(Math.random() * this.availableColors.length)];
@@ -21,24 +22,53 @@ class Dot {
     }
 }
 
+Dot.prototype.dotCount = 0;
+
 class Board {
     constructor() {
         this.boardSize = 5;
         this.dots = [];
-        for (let i = 0; i <= this.boardSize; i++) {
-            for (let j = 0; j <= this.boardSize; j++) {
-                new Dot;
+        for (let i = 0; i < this.boardSize; i++) {
+            this.dots.push([]);
+            for (let j = 0; j < this.boardSize; j++) {
+                this.dots[i][j] = new Dot();
             }
         }
+        this.selectionChain = [];
     }
-
-    // select(anArray) {
-
-    // }
-    // remove(anArray) {
-
-    // }
-    // move(anArray) {
-
+    removeDot(x, y) {
+        this.dots[x, y] = null;
+    }
+    slideDownFrom(rIndex, cIndex) {
+        for (let currRow = rIndex - 1; currRow >= 0; currRow--) {
+            this.dots[currRow + 1][cIndex] = this.dots[currRow][cIndex];
+        }
+    }
+    isAdjacentTo(x1, y1, x2, y2) {
+        if (
+            x1 === x2 && y1 === y2 - 1 ||
+            x1 === x2 && y1 === y2 + 1 ||
+            x1 === x2 + 1 && y1 === y2 ||
+            x1 === x2 - 1 && y1 === y2
+        ) {
+            return true
+        }
+    }
+    addSelection(x, y) {
+        this.selectionChain.push([]);
+        this.selectionChain.push(x, y)
+    }
+    // canBeSelected(x, y) {
+    //     if (selectionChain === []) {
+    //         return true;
+    //     } else if (
+    //         selectionChain !== [] &&
+    //         this.isAdjacentTo() === true &&
+    //         this.dots[x, y] === this.selectionChain[]
+    //     ) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
     // }
 }
